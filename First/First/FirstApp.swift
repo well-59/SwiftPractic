83e029ac83cd3f712b3350dfc6d5a453f7a6369f
well <73276854+well-59@users.noreply.github.com>
2021-06-15 18:23:31 +0800

    1

---
+++ First/First/FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstApp.swift
+//  First
+//
+//  Created by 黃士豪 on 2021/6/15.
+//
+
+import SwiftUI
+
+@main
+struct FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

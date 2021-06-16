9712952d0ac1587d498e3bc6b70c785a25b639bd
well <73276854+well-59@users.noreply.github.com>
2021-06-16 17:26:52 +0800

    0616Button

--- SwiftUIButton/SwiftUIButton/ContentView.swift
+++ SwiftUIButton/SwiftUIButton/ContentView.swift
@@ -9,13 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+        Button(action:{
+                print("Share tapped!")
+        }){
+            HStack{
+                Image(systemName: "square.and.arrow.up")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Text("Share")
+                    .fontWeight(.semibold)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            }
+        }.buttonStyle(GradientBackgroundStyle())
+            
+        Button(action:{
+                print("Edit tapped!")
+        }){
+            HStack{
+                Image(systemName: "square.and.pencil")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Text("Edit")
+                    .fontWeight(.semibold)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            }
+        }.buttonStyle(GradientBackgroundStyle())
+            
+        Button(action:{
+                print("Delete tapped!")
+        }){
+            HStack{
+                Image(systemName: "trash")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Text("Delete")
+                    .fontWeight(.semibold)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            }
+        }.buttonStyle(GradientBackgroundStyle())
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+struct GradientBackgroundStyle:ButtonStyle {
+    func makeBody(configuration:Self.Configuration) -> some View {
+        configuration.label
+            .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity)
+            .padding()
+            .foregroundColor(.white)
+            .background(LinearGradient(gradient: Gradient(colors: [Color("DarkGreen"), Color("LightGreen")]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
+            .cornerRadius(40)
+            .padding(.horizontal,20)
+            .scaleEffect(configuration.isPressed ? 0.9:1.0)
+            
     }
 }

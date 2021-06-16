2677fb82ae2a13ce0b4056c00789f32879ae15e1
well <73276854+well-59@users.noreply.github.com>
2021-06-16 14:12:53 +0800

    SwiftUIButton

---
+++ SwiftUIButton/SwiftUIButton/SwiftUIButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIButtonApp.swift
+//  SwiftUIButton
+//
+//  Created by 黃士豪 on 2021/6/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

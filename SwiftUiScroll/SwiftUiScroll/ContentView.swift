ffd774ba6828987e46cbc94aecbcba39860bd56f
well <73276854+well-59@users.noreply.github.com>
2021-06-16 14:18:55 +0800

    SwiftUiScroll

---
+++ SwiftUiScroll/SwiftUiScroll/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiScroll
+//
+//  Created by 黃士豪 on 2021/6/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUiScroll/SwiftUiScroll/SwiftUiScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiScrollApp.swift
+//  SwiftUiScroll
+//
+//  Created by 黃士豪 on 2021/6/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
